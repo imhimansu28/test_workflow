c6be7df8eefed2d4c0b7755e99bab29b69ad799f
imhimansu28 <imhimansu28@gmail.com>
2022-08-19 13:13:28 +0530

    c

--- main.py
+++ main.py
@@ -30,12 +30,12 @@ b = 9
 if __name__ == "__main__":
     logger.info(f"Token value: {SOME_SECRET}")
 
-    r = requests.get(
-        'https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
-    if r.status_code == 200:
-        data = r.json()
-        temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Berlin: {temperature}')
+    # r = requests.get(
+    #     'https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
+    # if r.status_code == 200:
+    #     data = r.json()
+    #     temperature = data["forecast"]["temp"]
+    #     logger.info(f'Weather in Berlin: {temperature}')
 
     if a == b:
         logger.info("a is equal to b")
